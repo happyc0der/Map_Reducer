d2ea6ba6f7899dfb6bbed0f191976a8d18d6b90b
happyc0der <KeshavRajput.4@gmail.com>
2024-04-17 23:08:13 +0530

    Handle 3rd GRPC call

--- Reducer.py
+++ Reducer.py
@@ -6,14 +6,14 @@ from concurrent import futures
 import threading 
 REDUCER_ID = -1
 RESPONSES = []
+NUM_MAPPERS = -1
 lock = threading.Lock()
 
-def send_reduce_request_to_mapper(mapper_port_number):
-
+def send_reduce_request_to_mapper(mapper_port_number, partitions_responsible_for):
     address = f'localhost:{mapper_port_number}'
     with grpc.insecure_channel(address) as channel:
         stub = mapreduce_pb2_grpc.MapReduceServiceStub(channel)
-        reduce_request = mapreduce_pb2.ReduceRequest(reducer_id=REDUCER_ID)
+        reduce_request = mapreduce_pb2.ReduceRequest(partitions=partitions_responsible_for)
         try:
             response = stub.Reduce(reduce_request)
             print("🔴 Recieved a Reduce Request!")
@@ -24,32 +24,51 @@ def send_reduce_request_to_mapper(mapper_port_number):
             print("❌ Error in Reduce Request") # according to sir this error will never happen
     return 
 def handle_start_reduce_request(response):
-    num_of_mappers = response.num_mappers
+    NUM_MAPPERS = response.num_mappers
+    responsible_for = response.partitions 
 
     mapper_port_number = []
-    for i in range(num_of_mappers):
+    for i in range(NUM_MAPPERS):
         mapper_port_number.append(4040 + (i+1))
     
     threads = []
-    for i in range(num_of_mappers):
-        thread = threading.Thread(target=send_reduce_request_to_mapper, args=(mapper_port_number[i],))
+    for i in range(NUM_MAPPERS):
+        thread = threading.Thread(target=send_reduce_request_to_mapper, args=(mapper_port_number[i],responsible_for))
         thread.start()
-        
         threads.append(thread)
+    
     for thread in threads:
         thread.join()
-
+    Collecting_Centroid_Data = {}
     for response in RESPONSES:
-        pass 
+        key = response.centroid_values.key 
+        values = response.centroid_values.values
+        if key not in Collecting_Centroid_Data:
+            Collecting_Centroid_Data[key] = values
+        else:
+            Collecting_Centroid_Data[key].extend(values)
+            
+    final_centroids = []
+    # NOT SURE THIS WORKS OR NOT
+    for key in Collecting_Centroid_Data:
+        sum_x = 0
+        sum_y = 0
+        count = 0
+        for values in Collecting_Centroid_Data[key]:
+            sum_x += values.x
+            sum_y += values.y
+            count += 1
+        final_centroids.append([key, mapreduce_pb2.Point(x=sum_x/count, y=sum_y/count)])
+
     
 
 
 
 class MapReduceServiceServicer(mapreduce_pb2_grpc.MapReduceServiceServicer):
     def StartReduce(self, request, context):
-        old_response = mapreduce_pb2.StartReduceResponse(ok=True)
         handle_start_reduce_request(request)
-        return old_response
+        response = mapreduce_pb2.ReduceResponse(status="OK")
+        return response
 
 if __name__ == "__main__":
     reducer_index = int(sys.argv[1]) + 1 # 1 - indexed
